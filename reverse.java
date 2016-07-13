dd85cf07acca3b77cc7def4f7bdb888ff7e2b539
Kazi Shahjahan <kazishahjahan@NRKL-0229.local>
2016-07-13 15:12:07 -0400

    Added html

---
+++ reverse.java
@@ -0,0 +1,22 @@
+public class reverse
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+			String [] s = sc.nextLine();
+			String [] words = s.split("");
+
+			for(int i = words.length-1; i <= 0; i--)
+			{
+				System.out.print("reverse is" + words[i]);
+			}
+
+			for(int i = 0; i < A.length; i++)
+			{
+				sysout("app");
+				more++;
+			}
+
+	}
+}
